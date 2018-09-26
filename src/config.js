1f5c8d69fe464f7a233299fd8f48df651dbcf8ce
VTI\Parker_Darren <darren.parker@videojet.com>
2018-09-26 14:40:37 +0100

    Adding stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const dev = {
+  STRIPE_KEY: "pk_test_hhQxTXh35W6WuA8BpkiV4HSR",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-1rx6qbtipfqsd"
@@ -15,6 +16,7 @@ const dev = {
   }
 };
 const prod = {
+  STRIPE_KEY: "pk_test_hhQxTXh35W6WuA8BpkiV4HSR",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-vbbu03d9pgzx"
